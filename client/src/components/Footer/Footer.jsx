f6dfa4a7c206eb1e024ec3ec1f948cb5e3cfdfe6
Mehron Talebi <55270572+mehront@users.noreply.github.com>
2023-11-28 00:25:13 -0500

    added updated components

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,17 +1,82 @@
 // components/Footer/Footer.jsx
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+
 import './Footer.less';
 
-const Footer = () => {
-  const navigate = useNavigate();
+const Footer = ({textScale, textScaleUpdate}) => {
+    const navigate = useNavigate();
 
-  return (
+    function increase(e) {
+      e.preventDefault();
+      
+      const test = textScale;
+  
+      console.log("Increase called: " + test);
+      textScaleUpdate(textScale + 0.25);
+      console.log("Increase end: " + (test + 0.25));
+  
+    }
+  
+    function decrease(e) {
+      if(textScale != 1) {
+        e.preventDefault();
+  
+        const test = textScale;
+        
+        console.log("Decrease called: " + test);
+        textScaleUpdate(textScale - 0.25);
+        console.log("Decrease end: " + (test - 0.25));
+  
+      }
+      else {
+        console.log(textScale);
+      }
+    }
+  
+    return (
     <footer className="footer">
-      <div className="footer-links">
-        <a onClick={() => navigate('/about')}>ABOUT</a>
-        <a onClick={() => navigate('/faq')}>FAQ</a>
-        <a onClick={() => navigate('/help')}>HELP</a>
+      <div className="footer-content">
+        
+        <div className="footer-section">
+          <h4>FOR PARENTS</h4>
+          <a href="#">How it works</a>
+          <a href="#">FAQ</a>
+          <a href="#">Start a Free Trial</a>
+          <a href="#">Login</a>
+        </div>
+        <div className="footer-section">
+          <h4>FOR EDUCATORS</h4>
+          <a href="#">Educator Resources</a>
+          <a href="#">Educator Login</a>
+          <a href="#">Professional Development</a>
+        </div>
+        <div className="footer-section">
+          <h4>CODESPARKS</h4>
+          <a href="#">About Us</a>
+          <a href="#">Awards</a>
+          <a href="#">Press</a>
+          <a href="#">Contact Us</a>
+          <a href="#">Blog</a>
+        </div>
+        <div className="footer-section">
+          <h4>PARTNERS</h4>
+          <a href="#">University of Florida</a>
+          <a href="#">National Science Foundation</a>
+          <a href="#">Texas A&M University</a>
+        </div>
+        <div className="footer-section">
+          <h4>ACCESSIBILITY</h4>
+          <a onClick={increase}>Increase Text Size</a>
+          <a onClick={decrease}>Decrease Text Size</a>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <span>© CodeSparks 2023</span>
+        <a href="#">Terms</a>
+        <a href="#">Privacy</a>
+        <a href="#">Do Not Sell My Personal Information</a>
       </div>
     </footer>
   );
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,18 +1,51 @@
 // components/Header/Header.jsx
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import './Header.less';
-import placeholderLogo from '../../assets/chasm.png'; // Make sure to add a placeholder logo image in your assets folder
+import placeholderLogo from '../../assets/chasm.png'; // Replace with your actual logo
 
 const Header = () => {
-  const navigate = useNavigate();
+  const navigate = (path) => {
+    // Navigation logic here
+    console.log(`Navigate to ${path}`); // Placeholder for navigation logic
+  };
 
   return (
     <header className="header">
-      <img src={placeholderLogo} alt="Placeholder Logo" className="header-logo" onClick={() => navigate('/')} />
-      <div className="header-content">
-        <button onClick={() => navigate('/join')} className="login-button">LOGIN</button>
-      </div>
+      <img src={placeholderLogo} alt="Logo" className="header-logo" onClick={() => navigate('/')} />
+      <nav className="header-navigation">
+        {/* Dropdown for Parents */}
+        <div className="dropdown">
+          <button className="dropbtn">Parents</button>
+          <div className="dropdown-content">
+            <a onClick={() => navigate('/how-it-works')}>How it Works</a>
+            <a onClick={() => navigate('/faq')}>FAQ</a>
+            <a onClick={() => navigate('/free-trial')}>Free Trial</a>
+            <a onClick={() => navigate('/login')}>Login</a>
+          </div>
+        </div>
+        {/* Dropdown for Educators */}
+        <div className="dropdown">
+          <button className="dropbtn">Educators</button>
+          <div className="dropdown-content">
+            <a onClick={() => navigate('/resources')}>Resources</a>
+            <a onClick={() => navigate('/teacher-dashboard')}>Teacher Dashboard</a>
+            <a onClick={() => navigate('/professional-development')}>Professional Development</a>
+            <a onClick={() => navigate('/play-online')}>Play Online</a>
+          </div>
+        </div>
+        {/* Existing Dropdown for About */}
+        <div className="dropdown">
+          <button className="dropbtn">About</button>
+          <div className="dropdown-content">
+            <a onClick={() => navigate('/about')}>About Us</a>
+            <a onClick={() => navigate('/awards')}>Awards</a>
+            <a onClick={() => navigate('/press')}>Press</a>
+            <a onClick={() => navigate('/contact')}>Contact Us</a>
+          </div>
+        </div>
+        <button onClick={() => navigate('/login')} className="login-button">LOGIN</button>
+        <button onClick={() => navigate('/free-trial')} className="trial-button">JOIN A SESSION</button>
+      </nav>
     </header>
   );
 };
